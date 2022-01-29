31b91368a3dc275e259298e2b1c86860709f2661
Brais <81094589+braiso-22@users.noreply.github.com>
2022-01-29 18:03:29 +0100

    agregadas etiquetas de puntuacion

--- Conecta4/src/view/View.java
+++ Conecta4/src/view/View.java
@@ -5,17 +5,91 @@
  */
 package view;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import model.Game;
+
 /**
  *
  * @author Brais
  */
 public class View extends javax.swing.JFrame {
 
+    Game partida;
+
     /**
      * Creates new form View
      */
     public View() {
+        partida = new Game();
         initComponents();
+        int filas = 6, columnas = 7;
+        lQuienToca = new JLabel("Le toca al jugador: Rojo", SwingConstants.CENTER);
+        lQuienToca.setFont(new java.awt.Font("Roboto", 0, 36));
+        lQuienToca.setForeground(Color.white);
+        pTurno.add(lQuienToca);
+        pTablero.setLayout(new java.awt.GridLayout(filas, columnas));
+        botones = new JButton[filas][columnas];
+        llenarBotones(filas, columnas);
+        this.setVisible(true);
+
+    }
+
+    public void applyNimbus() {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(View.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(View.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(View.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(View.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+    }
+
+    private void llenarBotones(int filas, int columnas) {
+        for (int i = filas - 1; i >= 0; i--) {
+            for (int j = 0; j < columnas; j++) {
+                botones[i][j] = new JButton();
+                botones[i][j].setName(i + " " + j);
+                pTablero.add(botones[i][j]);
+
+                final int iFinal = i, jFinal = j;
+                botones[i][j].addActionListener(new java.awt.event.ActionListener() {
+
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        FActionPerformed(e);
+                    }
+                });
+            }
+        }
+    }
+
+    private void FActionPerformed(ActionEvent evt) {
+        String[] casillas = ((JButton) evt.getSource()).getName().split(" ");
+        System.out.println(casillas[0] + " " + casillas[1]);
+        int fila = partida.addToColumna(Integer.parseInt(casillas[1]));
+        if (fila!=-1) {
+            if (partida.getTurno() == partida.ROJO) {
+                lQuienToca.setText("Le toca al jugador: Rojo");
+                botones[fila][Integer.parseInt(casillas[1])].setBackground(Color.YELLOW);
+            } else {
+                botones[fila][Integer.parseInt(casillas[1])].setBackground(Color.red);
+                lQuienToca.setText("Le toca al jugador: Amarillo");
+            }
+
+        }
     }
 
     /**
@@ -28,19 +102,69 @@ public class View extends javax.swing.JFrame {
     private void initComponents() {
 
         panelFondo = new javax.swing.JPanel();
+        pLateralIzquierda = new javax.swing.JPanel();
+        lPuntosRojo = new javax.swing.JLabel();
+        pLateralDerecha = new javax.swing.JPanel();
+        lPuntosAmarillo = new javax.swing.JLabel();
+        pTablero = new javax.swing.JPanel();
+        pTurno = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
 
-        javax.swing.GroupLayout panelFondoLayout = new javax.swing.GroupLayout(panelFondo);
-        panelFondo.setLayout(panelFondoLayout);
-        panelFondoLayout.setHorizontalGroup(
-            panelFondoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 960, Short.MAX_VALUE)
+        panelFondo.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        pLateralIzquierda.setBackground(new java.awt.Color(255, 255, 255));
+
+        lPuntosRojo.setFont(new java.awt.Font("Roboto Black", 0, 24)); // NOI18N
+        lPuntosRojo.setText("  Rojo: 0");
+
+        javax.swing.GroupLayout pLateralIzquierdaLayout = new javax.swing.GroupLayout(pLateralIzquierda);
+        pLateralIzquierda.setLayout(pLateralIzquierdaLayout);
+        pLateralIzquierdaLayout.setHorizontalGroup(
+            pLateralIzquierdaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pLateralIzquierdaLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lPuntosRojo, javax.swing.GroupLayout.DEFAULT_SIZE, 158, Short.MAX_VALUE)
+                .addContainerGap())
         );
-        panelFondoLayout.setVerticalGroup(
-            panelFondoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 540, Short.MAX_VALUE)
+        pLateralIzquierdaLayout.setVerticalGroup(
+            pLateralIzquierdaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pLateralIzquierdaLayout.createSequentialGroup()
+                .addGap(10, 10, 10)
+                .addComponent(lPuntosRojo, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(478, Short.MAX_VALUE))
         );
+
+        panelFondo.add(pLateralIzquierda, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 170, 540));
+
+        pLateralDerecha.setBackground(new java.awt.Color(255, 255, 255));
+
+        lPuntosAmarillo.setFont(new java.awt.Font("Roboto Black", 0, 24)); // NOI18N
+        lPuntosAmarillo.setText("  Amarillo: 0");
+
+        javax.swing.GroupLayout pLateralDerechaLayout = new javax.swing.GroupLayout(pLateralDerecha);
+        pLateralDerecha.setLayout(pLateralDerechaLayout);
+        pLateralDerechaLayout.setHorizontalGroup(
+            pLateralDerechaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(lPuntosAmarillo, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 170, Short.MAX_VALUE)
+        );
+        pLateralDerechaLayout.setVerticalGroup(
+            pLateralDerechaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pLateralDerechaLayout.createSequentialGroup()
+                .addComponent(lPuntosAmarillo, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 488, Short.MAX_VALUE))
+        );
+
+        panelFondo.add(pLateralDerecha, new org.netbeans.lib.awtextra.AbsoluteConstraints(790, 0, 170, 540));
+
+        pTablero.setBackground(new java.awt.Color(0, 102, 255));
+        pTablero.setLayout(new java.awt.GridLayout(1, 0));
+        panelFondo.add(pTablero, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 0, 620, 410));
+
+        pTurno.setBackground(new java.awt.Color(0, 51, 255));
+        pTurno.setLayout(new java.awt.GridLayout(1, 0));
+        panelFondo.add(pTurno, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 410, 620, 130));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -54,6 +178,7 @@ public class View extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     /**
@@ -92,6 +217,14 @@ public class View extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel lPuntosAmarillo;
+    private javax.swing.JLabel lPuntosRojo;
+    private javax.swing.JPanel pLateralDerecha;
+    private javax.swing.JPanel pLateralIzquierda;
+    private javax.swing.JPanel pTablero;
+    private javax.swing.JPanel pTurno;
     private javax.swing.JPanel panelFondo;
     // End of variables declaration//GEN-END:variables
+    private JButton[][] botones;
+    JLabel lQuienToca;
 }
