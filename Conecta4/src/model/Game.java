3b10b8028cd17315289f1a906f863e02e6e93ea3
Brais <81094589+braiso-22@users.noreply.github.com>
2022-01-29 13:22:19 +0100

    constructores, iniciado de casillas y meter ficha

--- Conecta4/src/model/Game.java
+++ Conecta4/src/model/Game.java
@@ -10,5 +10,67 @@ package model;
  * @author Brais
  */
 public class Game {
+
+    public final int VACIO = 0;
+    public final int AMARILLO = 1;
+    public final int ROJO = 2;
+    private int[][] casillas;
+    private int filas;
+    private int columnas;
+    private int turno;
+
+    public Game(int filas, int columnas) {
+        turno = ROJO;
+        this.filas = filas;
+        this.columnas = columnas;
+        this.casillas = new int[this.filas][this.columnas];
+        iniciarCasillas();
+
+    }
+
+    public Game() {
+        turno = ROJO;
+        filas = 6;
+        columnas = 7;
+        this.casillas = new int[this.filas][this.columnas];
+        iniciarCasillas();
+    }
+
+    private void iniciarCasillas() {
+        for (int i = filas - 1; i > 0; i--) {
+            for (int j = 0; j < columnas; j++) {
+                casillas[i][j] = VACIO;
+            }
+        }
+    }
+
+    public boolean addToColumna(int col) {
+        boolean encontrado = false;
+        for (int i = 0; i < filas && !encontrado; i++) {
+
+            if (casillas[i][col] == VACIO) {
+                casillas[i][col] = turno;
+                cambiarTurno();
+                encontrado = true;
+            }
+        }
+        return encontrado;
+    }
+
+    private void cambiarTurno() {
+        if (turno == ROJO) {
+            turno = AMARILLO;
+        } else {
+            turno = ROJO;
+        }
+    }
+
+    public boolean finalizado() {
+        return lineaHorizontal();
+    }
     
+    private boolean lineaHorizontal(){
+        return false;
+    }
+
 }
